254d032bd8fe36ba6bd5bd2cd222fc0e86a9fa03
csmaslin <csmaslin@gmail.com>
2026-08-22 19:23:46 -0700

    Bump to v2.06 and add changelog

--- src/CvarcLogger.App/AppVersion.cs
+++ src/CvarcLogger.App/AppVersion.cs
@@ -6,5 +6,5 @@ namespace CvarcLogger.App;
 /// gets published to the user.</summary>
 public static class AppVersion
 {
-    public const string Current = "2.05";
+    public const string Current = "2.06";
 }
