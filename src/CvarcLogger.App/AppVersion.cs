00e42069420f33d653a00763b699d6c2f08a4235
csmaslin <csmaslin@gmail.com>
2026-08-25 18:39:32 -0700

    Bump version to 2.08, add changelog entry for SKCC lookup/badge/exchange-preview/scoring

--- src/CvarcLogger.App/AppVersion.cs
+++ src/CvarcLogger.App/AppVersion.cs
@@ -6,5 +6,5 @@ namespace CvarcLogger.App;
 /// gets published to the user.</summary>
 public static class AppVersion
 {
-    public const string Current = "2.07";
+    public const string Current = "2.08";
 }
